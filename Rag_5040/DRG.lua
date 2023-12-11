03e98bd140f9e106eb16ee47f68851785198b01a
Rag <genshinteambuilds@gmail.com>
2023-12-11 09:48:51 -0800

    Add DRG

---
+++ Rag_5040/DRG.lua
@@ -0,0 +1,127 @@
+--[[
+    This is provided purely as an example template. Only very basic sanity testing has been done.
+]]
+
+local profile = {}
+
+local fastCastValue = 0.00 -- 0% from gear
+
+local ethereal_earring = true
+local ethereal_earring_slot = 'Ear2'
+
+local sets = {
+    Idle = {},
+    IdleALT = {},
+    Resting = {},
+    Town = {},
+    Movement = {},
+
+    DT = {},
+    MDT = { -- Shell IV provides 23% MDT
+    },
+    FireRes = {},
+    IceRes = {},
+    LightningRes = {},
+    EarthRes = {},
+    WindRes = {},
+    WaterRes = {},
+    Evasion = {},
+
+    Precast = {}, -- Not Required for DRG. This can be left empty.
+    SIRD = { -- 102% to Cap
+    },
+    Haste = { -- Used for Utsusemi cooldown
+    },
+
+    LockSet1 = {},
+    LockSet2 = {},
+    LockSet3 = {},
+
+    TP_LowAcc = {},
+    TP_HighAcc = {},
+
+    MaxHP = {},
+    BreathBonus = {},
+
+    ['Ancient Circle'] = {},
+    ['Jump'] = {},
+    ['High Jump'] = {},
+    ['Super Jump'] = {},
+    ['Call Wyvern'] = {},
+    ['Spirit Link'] = {},
+
+    WS = {},
+    ['Penta Thrust'] = {},
+    ['Wheeling Thrust'] = {},
+    ['Impulse Drive'] = {},
+    ['Skewer'] = {},
+    ['Geirskogul'] = {},
+}
+profile.Sets = sets
+
+profile.SetMacroBook = function()
+    AshitaCore:GetChatManager():QueueCommand(1, '/macro book 1')
+    AshitaCore:GetChatManager():QueueCommand(1, '/macro set 1')
+end
+
+profile.HandleAbility = function()
+    local action = gData.GetAction()
+    if (action.Name == "Steady Wing") then
+        gFunc.EquipSet(sets.BreathBonus)
+    elseif (JobAbilities:contains(action.Name)) then
+        gFunc.EquipSet(sets[action.Name])
+    end
+end
+
+profile.HandleWeaponskill = function()
+    gFunc.EquipSet(sets.WS)
+
+    local action = gData.GetAction()
+    if (WeaponSkills:contains(action.Name)) then
+      gFunc.EquipSet(sets[action.Name])
+    end
+end
+
+gcmelee = gFunc.LoadFile('common\\gcmelee.lua')
+
+profile.OnLoad = function()
+    gcmelee.Load()
+    profile.SetMacroBook()
+end
+
+profile.OnUnload = function()
+    gcmelee.Unload()
+end
+
+profile.HandleCommand = function(args)
+    gcmelee.DoCommands(args)
+end
+
+profile.HandleDefault = function()
+    gcmelee.DoDefault()
+
+    local petAction = gData.GetPetAction()
+    if (petAction ~= nil) then
+        gFunc.EquipSet(sets.BreathBonus)
+        return
+    end
+
+    gcmelee.DoDefaultOverride()
+
+    local player = gData.GetPlayer()
+    if (ethereal_earring == true and (player.SubJob == 'WHM' or player.SubJob == 'RDM')) then
+        gFunc.Equip(ethereal_earring_slot, 'Ethereal Earring')
+    end
+end
+
+profile.HandlePrecast = function()
+    gcmelee.DoPrecast(fastCastValue)
+    gFunc.EquipSet(sets.MaxHP)
+end
+
+profile.HandleMidcast = function()
+    gcmelee.DoMidcast(sets)
+    gFunc.EquipSet(sets.MaxHP)
+end
+
+return profile
