a96890f5f49edd84a84a712999f0f08b8045e676
Rag <genshinteambuilds@gmail.com>
2024-02-01 20:31:35 -0800

    Fix stupid horizon banish III cast time

--- common/gcmage.lua
+++ common/gcmage.lua
@@ -372,6 +372,9 @@ function gcmage.SetupMidcastDelay(fastCastValue)
     if (action.Type == 'Bard Song' and string.match(action.Name, 'Prelude')) then
         castTime = 8000
     end
+    if (action.Skill == 'Divine Magic' and action.Name == 'Banish III')) then
+        castTime = 3000
+    end
     local minimumBuffer = 0.25 -- Can be lowered to 0.1 if you want
     local packetDelay = 0.25 -- Change this to 0.4 if you do not use PacketFlow
     local castDelay = ((castTime * (1 - fastCastValue)) / 1000) - minimumBuffer
