6876e325abd94742e5812a3b1a0c15c7d3dee579
yzyii <mark.yz.yii@gmail.com>
2024-03-06 15:30:03 -0800

    Update DRK.lua

--- Rag_5040/DRK.lua
+++ Rag_5040/DRK.lua
@@ -1,6 +1,8 @@
 --[[
-    This is provided purely as an example template. Only very basic sanity testing has been done.
+    This is provided purely as an example. No testing has been done.
 ]]
+
+local use_chaos_burgeonet_for_tp_during_souleater = false
 
 local profile = {}
 
@@ -22,12 +24,12 @@ local sets = {
     EarthRes = {},
     WindRes = {},
     WaterRes = {},
-    Evasion = {},
+    Evasion = {}, -- Use this set for your zerg set. See README.md
 
     Precast = {},
     SIRD = { -- 102% to Cap
     },
-    Haste = { -- Used for Utsusemi cooldown
+    Haste = { -- Used for Utsusemi and Stun cooldown
     },
 
     LockSet1 = {},
@@ -38,6 +40,17 @@ local sets = {
     TP_HighAcc = {},
 
     WS = {},
+    WS_Guillotine = {},
+    WS_SpinningSlash = {},
+    WS_CrossReaper = {},
+
+    WeaponBash = {},
+    ArcaneCircle = {},
+    SoulEater = {},
+    Nuke = {},
+    Enfeebling = {},
+    Drain = {},
+    Absorb = {},
 }
 profile.Sets = sets
 
@@ -49,7 +62,12 @@ end
 gcmelee = gFunc.LoadFile('common\\gcmelee.lua')
 
 profile.HandleAbility = function()
-    -- You may add logic here
+    local action = gData.GetAction();
+    if (action.Name == 'Weapon Bash') then 
+        gFunc.EquipSet(sets.WeaponBash)
+    elseif (action.Name == 'Arcane Circle') then 
+        gFunc.EquipSet(sets.ArcaneCircle)
+    end
 end
 
 profile.HandleItem = function()
@@ -67,7 +85,23 @@ end
 
 profile.HandleWeaponskill = function()
     gFunc.EquipSet(sets.WS)
-    -- You may add logic here
+
+    local action = gData.GetAction()
+    if (action.Name == 'Guillotine') then
+        gFunc.EquipSet(sets.WS_Guillotine)
+    elseif (action.Name == 'Spinning Slash') then
+        gFunc.EquipSet(sets.WS_SpinningSlash)
+    elseif (action.Name == 'Cross Reaper') then
+        gFunc.EquipSet(sets.WS_CrossReaper)
+    end
+
+    local souleater = gData.GetBuffCount('Souleater')
+    if (gcdisplay.IdleSet == 'Evasion') then
+        gFunc.EquipSet(sets.Evasion)
+    end
+    if (souleater > 0) then
+        gFunc.EquipSet(sets.SoulEater)
+    end
 end
 
 profile.OnLoad = function()
@@ -88,7 +122,12 @@ end
 
 profile.HandleDefault = function()
     gcmelee.DoDefault()
-    -- You may add logic here
+
+    local souleater = gData.GetBuffCount('Souleater')
+    if (souleater > 0 and player.Status == 'Engaged' and use_chaos_burgeonet_for_tp_during_souleater) then
+        gFunc.EquipSet(sets.SoulEater)
+    end
+
     gcmelee.DoDefaultOverride()
     gFunc.EquipSet(gcinclude.BuildLockableSet(gData.GetEquipment()))
 end
@@ -100,7 +139,20 @@ end
 
 profile.HandleMidcast = function()
     gcmelee.DoMidcast(sets)
-    -- You may add logic here
+
+    local action = gData.GetAction();
+    if (action.Skill == 'Elemental Magic') then
+        gFunc.EquipSet(sets.Nuke)
+    elseif (action.Skill == 'Enfeebling Magic') then
+        gFunc.EquipSet(sets.Enfeebling)
+    elseif (action.Skill == 'Dark Magic') then
+        gFunc.EquipSet(sets.Drain)
+        if (string.contains(spell.Name, 'Absorb')) then
+            gFunc.EquipSet(sets.Absorb)
+        elseif (string.contains(spell.Name, 'Stun')) then
+            gFunc.EquipSet(sets.Haste)
+        end
+    end
 end
 
 return profile
