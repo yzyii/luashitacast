7b0e17a56f7969dd07d01168a00816b88b1a927d
EUPH\marky <mark.yz.yii@gmail.com>
2025-06-02 01:16:09 -0700

    Increase thresholds on interim delays for lag

--- common/gcmage.lua
+++ common/gcmage.lua
@@ -521,8 +521,8 @@ function gcmage.SetupMidcastDelay(fastCastValue)
     if (action.Skill == 'Divine Magic' and action.Name == 'Banish III') then
         castTime = 3000
     end
-    local minimumBuffer = 0.25 -- Can be lowered to 0.1 if you want
-    local packetDelay = 0.25 -- Change this to 0.4 if you do not use PacketFlow
+    local minimumBuffer = 0.4 -- Can be lowered to 0.1 if you want
+    local packetDelay = 0.4 -- Change this to 0.4 if you do not use PacketFlow
     local castDelay = ((castTime * (1 - fastCastValue)) / 1000) - minimumBuffer
     if (castDelay >= packetDelay) then
         gFunc.SetMidDelay(castDelay)
--- common/gcmelee.lua
+++ common/gcmelee.lua
@@ -174,8 +174,8 @@ function gcmelee.SetupMidcastDelay(fastCastValue)
     if (player.SubJob == "RDM") then
          fastCastValue = fastCastValue + 0.15 -- Fast Cast Trait
     end
-    local minimumBuffer = 0.25 -- Can be lowered to 0.1 if you want
-    local packetDelay = 0.25 -- Change this to 0.4 if you do not use PacketFlow
+    local minimumBuffer = 0.4 -- Can be lowered to 0.1 if you want
+    local packetDelay = 0.4 -- Change this to 0.4 if you do not use PacketFlow
     local castDelay = ((castTime * castTimeMod * (1 - fastCastValue)) / 1000) - minimumBuffer
     if (castDelay >= packetDelay) then
         gFunc.SetMidDelay(castDelay)
