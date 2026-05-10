d89a15240e9b0cc1cad979b991842ef565117a4b
RAPTOP\marky <mark.yz.yii@gmail.com>
2026-05-10 17:13:39 +1000

    Upddate THF

--- Rag_5040/THF.lua
+++ Rag_5040/THF.lua
@@ -15,22 +15,96 @@ local evasion_master_casters_mitts = {
 }
 
 local sets = {
-    Idle = {},
-    IdleALT = {},
-    Resting = {},
+    Idle = {
+        Head = { Name = 'Rog. Bonnet +1', Priority = 20 },
+        Neck = 'Jeweled Collar +1',
+        Ear1 = 'Musical Earring',
+        Ear2 = 'Novia Earring',
+        Body = { Name = 'Scp. Harness +1', Priority = 20 },
+        Hands = { Name = 'War Gloves +1', Priority = 20 },
+        Ring1 = 'Shadow Ring',
+        Ring2 = { Name = 'Sattva Ring', Priority = 20 },
+        Back = 'Boxer\'s Mantle',
+        Waist = { Name = 'Scouter\'s Rope', Priority = -5 },
+        Legs = 'Raven Hose',
+        Feet = 'Dance Shoes +1',
+    },
+    IdleALT = {
+        Main = 'Terra\'s Staff',
+        Head = { Name = 'Rog. Bonnet +1', Priority = 20 },
+        Neck = 'Jeweled Collar +1',
+        Ear1 = 'Musical Earring',
+        Ear2 = 'Novia Earring',
+        Body = { Name = 'Scp. Harness +1', Priority = 20 },
+        Hands = { Name = 'War Gloves +1', Priority = 20 },
+        Ring1 = 'Shadow Ring',
+        Ring2 = { Name = 'Sattva Ring', Priority = 20 },
+        Back = 'Boxer\'s Mantle',
+        Waist = { Name = 'Scouter\'s Rope', Priority = -5 },
+        Legs = 'Raven Hose',
+        Feet = 'Dance Shoes +1',
+    },
+    Resting = {
+        Body = 'Nomad\'s Tunica',
+        Neck = 'Paisley Scarf',
+        Ear1 = 'Sanative Earring',
+        Hands = 'Nomad\'s Gloves',
+        Legs = 'Nomad\'s Hose',
+        Feet = 'Nomad\'s Boots',
+    },
     Town = {},
-    Movement = {},
-    Movement_TP = {},
+    Movement = {
+        Feet = 'Trotter Boots',
+    },
+    Movement_TP = {
+        Hands = { Name = 'Homam Manopolas', Priority = 30 },
+    },
 
-    DT = {},
-    MDT = {},
+    DT = {
+        Head = 'Darksteel Cap +1',
+        Neck = { Name = 'Evasion Torque', Priority = 20 },
+        Body = 'Dst. Harness +1',
+        Hands = 'Dst. Mittens +1',
+        Ring1 = 'Jelly Ring',
+        Ring2 = { Name = 'Sattva Ring', Priority = 20 },
+        Back = 'Shadow Mantle',
+        Legs = 'Dst. Subligar +1',
+        Feet = 'Dst. Leggings +1',
+    },
+    MDT = {
+        Head = { Name = 'Homam Zucchetto', Priority = 30 },
+        Neck = 'Jeweled Collar +1',
+        Ear1 = 'Merman\'s Earring',
+        Ear2 = 'Merman\'s Earring',
+        Body = { Name = 'Merman\'s Harness', Priority = 15 },
+        Hands = 'Merman\'s Bangles',
+        Ring1 = 'Shadow Ring',
+        Ring2 = { Name = 'Sattva Ring', Priority = 20 },
+        Back = 'Resentment Cape',
+        Waist = { Name = 'Powerful Rope', Priority = 30 },
+        Legs = { Name = 'Merman\'s Subligar', Priority = 15 },
+        Feet = { Name = 'Mmn. Leggings', Priority = 15 },
+    },
     FireRes = {},
     IceRes = {},
     LightningRes = {},
     EarthRes = {},
     WindRes = {},
     WaterRes = {},
-    Evasion = {},
+    Evasion = {
+        Head = 'Optical Hat',
+        Neck = 'Evasion Torque',
+        Ear1 = 'Musical Earring',
+        Ear2 = 'Novia Earring',
+        Body = { Name = 'Scp. Harness +1', Priority = 20 },
+        Hands = { Name = 'War Gloves +1', Priority = 20 },
+        Ring1 = 'Breeze Ring',
+        Ring2 = 'Nimble Ring',
+        Back = 'Boxer\'s Mantle',
+        Waist = { Name = 'Scouter\'s Rope', Priority = -5 },
+        Legs = 'Raven Hose',
+        Feet = 'Dance Shoes +1',
+    },
     Override = { -- An additional override set explicitly to be used for sets such as crafting, HELM, fishing, or any other special sets such as DRK 2HR, MNK Counter etc. n.b. Any unused Resist or Evasion set can be used similarly.
         Body = 'Field Tunica',
         Hands = 'Field Gloves',
@@ -38,26 +112,105 @@ local sets = {
         Feet = 'Field Boots'
     },
 
-    Precast = {},
-    SIRD = { -- Only used for Idle sets and not while Override sets are active
+    Precast = {
+        Ear2 = { Name = 'Loquac. Earring', Priority = 10 },
+        Legs = { Name = 'Homam Cosciales', Priority = 30 },
     },
-    Haste = { -- Used for Utsusemi cooldown
+    SIRD = { -- Override sets (Resistance / Evasion) take precedence if in use.
+        Head = 'Optical Hat',
+        Neck = 'Evasion Torque',
+        Ear1 = 'Musical Earring',
+        Ear2 = 'Novia Earring',
+        Body = { Name = 'Scp. Harness +1', Priority = 20 },
+        Hands = { Name = 'War Gloves +1', Priority = 20 },
+        Ring1 = 'Breeze Ring',
+        Ring2 = 'Nimble Ring',
+        Back = 'Boxer\'s Mantle',
+        Waist = { Name = 'Scouter\'s Rope', Priority = -5 },
+        Legs = 'Raven Hose',
+        Feet = 'Dance Shoes +1',
+    },
+    Haste = {
+        Head = { Name = 'Homam Zucchetto', Priority = 30 },
+        Ear1 = { Name = 'Loquac. Earring', Priority = 10 },
+        Body = 'Rapparee Harness',
+        Hands = { Name = 'Dusk Gloves +1', Priority = 20 },
+        Waist = 'Sonic Belt',
+        Legs = { Name = 'Homam Cosciales', Priority = 30 },
+        Feet = { Name = 'Homam Gambieras', Priority = 30 },
     },
 
-    LockSet1 = {},
-    LockSet2 = {},
-    LockSet3 = {},
-
-    TP_LowAcc = {},
+    TP_LowAcc = {
+        Head = { Name = 'Homam Zucchetto', Priority = 30 },
+        Neck = 'Love Torque',
+        Ear1 = 'Brutal Earring',
+        Ear2 = 'Merman\'s Earring',
+        Body = { Name = 'Rapparee Harness', Priority = -2 },
+        Hands = { Name = 'Dusk Gloves +1', Priority = 20 },
+        Ring1 = 'Flame Ring',
+        Ring2 = 'Triumph Ring',
+        Back = 'Forager\'s Mantle',
+        Waist = { Name = 'Sonic Belt', Priority = 2 },
+        Legs = { Name = 'Homam Cosciales', Priority = 30 },
+        Feet = { Name = 'Dusk Ledelsens +1', Priority = 20 }
+    },
     TP_Aftermath = {},
-    TP_Mjollnir_Haste = {},
-    TP_HighAcc = {},
-    TP_NIN = {},
+    TP_Mjollnir_Haste = {
+        Head = 'Maat\'s Cap',
+    },
+    TP_HighAcc = {
+        Body = { Name = 'Homam Corazza', Priority = 30 },
+        Ring1 = { Name = 'Toreador\'s Ring', Priority = 20 },
+        Ring2 = { Name = 'Toreador\'s Ring', Priority = 20 },
+        Feet = { Name = 'Homam Gambieras', Priority = 30 }
+    },
+    TP_NIN = {
+        Ear2 = 'Stealth Earring',
+    },
 
     -- Note that these sets are for naked SA/TA/SATAs without WS
-    SA = {},
-    TA = {},
-    SATA = {},
+    SA = {
+        Head = 'Maat\'s Cap',
+        Neck = 'Love Torque',
+        Ear1 = 'Brutal Earring',
+        Ear2 = 'Pixie Earring',
+        Body = 'Dragon Harness +1',
+        Hands = { Name = 'Dusk Gloves +1', Priority = 20 },
+        Ring1 = 'Thunder Ring',
+        Ring2 = 'Adroit Ring',
+        Back = 'Forager\'s Mantle',
+        Waist = 'Warwolf Belt',
+        Legs = { Name = 'Dusk Trousers +1', Priority = 20 },
+        Feet = { Name = 'Dusk Ledelsens +1', Priority = 20 },
+    },
+    TA = {
+        Head = 'Maat\'s Cap',
+        Neck = 'Hope Torque',
+        Ear1 = 'Drone Earring',
+        Ear2 = 'Drone Earring',
+        Body = 'Dragon Harness +1',
+        Hands = { Name = 'Rog. Armlets +1', Priority = 20 },
+        Ring1 = 'Breeze Ring',
+        Ring2 = 'Nimble Ring',
+        Back = 'Forager\'s Mantle',
+        Waist = 'Sonic Belt',
+        Legs = { Name = 'Dusk Trousers +1', Priority = 20 },
+        Feet = { Name = 'Dusk Ledelsens +1', Priority = 20 },
+    },
+    SATA = {
+        Head = 'Maat\'s Cap',
+        Neck = 'Love Torque',
+        Ear1 = 'Pixie Earring',
+        Ear2 = 'Drone Earring',
+        Body = 'Dragon Harness +1',
+        Hands = { Name = 'Rog. Armlets +1', Priority = 20 },
+        Ring1 = 'Thunder Ring',
+        Ring2 = 'Adroit Ring',
+        Back = 'Forager\'s Mantle',
+        Waist = 'Warwolf Belt',
+        Legs = { Name = 'Dusk Trousers +1', Priority = 20 },
+        Feet = { Name = 'Drn. Leggings +1', Priority = 20 },
+    },
 
     -- The following demonstrates layering of WS sets that should cover all debatable major WS combinations
     WS = {
@@ -104,7 +257,6 @@ local sets = {
     },
 
     -- The following are applied on both SA WS and SATA WS
-    -- The following are applied on both SA WS and SATA WS
     WS_SA = {
         Feet = { Name = 'Hct. Leggings +1', Priority = 20 },
     },
@@ -138,6 +290,7 @@ local sets = {
     },
     WS_TA_MercyStroke = {
         Ear2 = 'Drone Earring',
+        Hands = { Name = 'Hct. Mittens +1', Priority = 20 },
     },
     WS_TA_EnergyDrain = {
         Ear2 = 'Drone Earring',
@@ -160,25 +313,86 @@ local sets = {
         Feet = { Name = 'Hct. Leggings +1', Priority = 20 },
     },
 
-    Flee = {},
-    Hide = {},
-	Steal_HPDown = {},
-    Steal = {},
-    Mug = {},
-
-    TH = {},
+    Flee = {
+        Feet = 'Rogue\'s Poulaines',
+    },
+    Hide = {
+        Body = { Name = 'Rogue\'s Vest', Priority = 20 },
+    },
+    Mug = {
+        Head = { Name = 'Asn. Bonnet +1', Priority = 20 },
+        Legs = { Name = 'Asn. Culottes +1', Priority = 20 },
+    },
+    TH = {
+        Neck = 'Nanaa\'s Charm',
+        Hands = { Name = 'Asn. Armlets +1', Priority = 20 },
+    },
+    Steal_HPDown = {
+        Head = 'Optical Hat',
+        Neck = 'Jeweled Collar +1',
+        Ear1 = 'Musical Earring',
+        Ear2 = 'Novia Earring',
+        Body = 'Rapparee Harness',
+        Hands = 'Merman\'s Bangles',
+        Ring1 = 'Shadow Ring',
+        Ring2 = { Name = 'Ether Ring', Priority = 5 },
+        Back = 'Boxer\'s Mantle',
+        Waist = { Name = 'Scouter\'s Rope', Priority = -5 },
+        Legs = 'Raven Hose',
+        Feet = 'Dance Shoes +1',
+    },
+    Steal = { -- 1225 + 55 + 326 -> 1280 -> 1204 / 1606
+        Head = { Name = 'Rog. Bonnet +1', Priority = 20 },
+        Neck = 'Rabbit Charm',
+        Ear1 = { Name = 'Cassie Earring', Priority = 15 },
+        Ear2 = { Name = 'Pigeon Earring +1', Priority = 20 },
+        Body = { Name = 'Homam Corazza', Priority = 30 },
+        Hands = 'Thief\'s Kote',
+        Ring1 = 'Rogue\'s Ring',
+        Ring2 = { Name = 'Bomb Queen Ring', Priority = 20 },
+        Back = { Name = 'Gigant Mantle', Priority = 20 },
+        Waist = { Name = 'Powerful Rope', Priority = 30 },
+        Legs = { Name = 'Asn. Culottes +1', Priority = 20 },
+        Feet = { Name = 'Rogue\'s Poulaines', Priority = 20 },
+    },
 
     Preshot = {}, -- This set is pointless until ToAU+ when Snapshot on equipment is available
-    Ranged = {},
-    Ranged_INT = {},
+    Ranged = {
+        Range = 'Ziska\'s Crossbow',
+        Head = 'Optical Hat',
+        Neck = 'Peacock Amulet',
+        Ear1 = 'Drone Earring',
+        Ear2 = 'Drone Earring',
+        Body = 'Dragon Harness +1',
+        Hands = 'Dragon Mittens +1',
+        Ring1 = 'Behemoth Ring +1',
+        Ring2 = 'Behemoth Ring +1',
+        Back = 'Jaeger Mantle',
+        Waist = { Name = 'Scouter\'s Rope', Priority = -5 },
+        Legs = { Name = 'Dusk Trousers +1', Priority = 20 },
+        Feet = { Name = 'Homam Gambieras', Priority = 30 },
+    },
+    Ranged_INT = {
+        Head = 'Maat\'s Cap',
+        Neck = 'Prudence Torque',
+        Ear1 = 'Omn. Earring',
+        Ear2 = 'Omn. Earring',
+        Body = { Name = 'Blue Cotehard. +1', Priority = 5 },
+        Hands = 'Engineer\'s Gloves',
+        Ring1 = 'Snow Ring',
+        Ring2 = 'Omniscient Ring',
+        Back = { Name = 'Fed. Army Mantle', Priority = 10 },
+        Waist = 'Ryl.Kgt. Belt',
+        Feet = 'Mountain Gaiters',
+    },
 
     None = { -- Default range / ammo equipment while idle
-        -- Range = 'displaced',
-        -- Ammo = 'Bomb Core',
+        Range = 'displaced',
+        Ammo = 'Bomb Core',
     },
     None_RA = { -- Used for ranged attacks when None set is being used instead of the bolt sets defined below
-        -- Range = 'displaced',
-        -- Ammo = 'Pebble',
+        Range = 'displaced',
+        Ammo = 'Dart',
     },
     Acid = {
         Ammo = 'Acid Bolt',
@@ -196,7 +410,7 @@ local sets = {
         Ammo = 'Venom Bolt',
     },
 
-    Weapon_Loadout_1 = { -- Slug Shot THF 2H Zerg
+    LockSet1 = { -- Slug Shot THF 2H Zerg
         Head = { Name = 'Hecatomb Cap +1', Priority = 20 },
         Ear1 = 'Triumph Earring',
         Ear2 = 'Drone Earring',
@@ -209,10 +423,36 @@ local sets = {
         Legs = 'Republic Subligar',
         Feet = { Name = 'Hct. Leggings +1', Priority = 20 },
     },
-    Weapon_Loadout_2 = {},
-    Weapon_Loadout_3 = {},
+    LockSet2 = {},
+    LockSet3 = {},
 
-    VileElixir = {},
+    Weapon_Loadout_1 = {
+        Main = 'Blau Dolch',
+        Sub = 'X\'s Knife',
+    },
+    Weapon_Loadout_2 = {
+        Main = 'Blau Dolch',
+        Sub = 'Sirocco Kukri',
+    },
+    Weapon_Loadout_3 = {
+        Main = 'Blau Dolch',
+        Sub = 'Viking Shield',
+    },
+
+    VileElixir = {
+        Head = { Name = 'Homam Zucchetto', Priority = 30 },
+        Neck = 'Shield Pendant',
+        Ear1 = { Name = 'Cassie Earring', Priority = 15 },
+        Ear2 = { Name = 'Pigeon Earring +1', Priority = 20 },
+        Body = { Name = 'Homam Corazza', Priority = 30 },
+        Hands = { Name = 'Feronia\'s Bangles', Priority = 20 },
+        Ring1 = { Name = 'Bomb Queen Ring', Priority = 20 },
+        Ring2 = { Name = 'Sattva Ring', Priority = 20 },
+        Back = { Name = 'Gigant Mantle', Priority = 20 },
+        Waist = { Name = 'Powerful Rope', Priority = 30 },
+        Legs = { Name = 'Dusk Trousers +1', Priority = 20 },
+        Feet = { Name = 'Homam Gambieras', Priority = 30 },
+    },
 }
 
 profile.SetMacroBook = function()
@@ -266,7 +506,7 @@ profile.HandleAbility = function()
     elseif (action.Name == 'Hide') then
         gFunc.EquipSet(sets.Hide)
     elseif (action.Name == 'Steal') then
-		gFunc.ForceEquipSet(sets.Steal_HPDown)
+        gFunc.ForceEquipSet(sets.Steal_HPDown)
         gFunc.EquipSet(sets.Steal)
     elseif (action.Name == 'Mug') then
         gFunc.EquipSet(sets.Mug)
@@ -493,17 +733,17 @@ profile.WatchTreasureHunter = function()
         end
 
         if (e.id == 0x28) then
-            local type = { 
+            local type = {
                 [1] = true, -- Attack
                 [2] = true, -- Ranged Attack
-                [3] = true, -- Ability 
-                [4] = true, 
+                [3] = true, -- Ability
+                [4] = true,
                 [6] = true -- Also ability? (Provoke)
             };
             local packet = actionpacket:parse(e);
             if (packet.UserId == playerEntity.ServerId) then
                 if (type[packet.Type]) then
-                    local reaction = { 
+                    local reaction = {
                         [0] = true, -- Spell Hit / ???
                         [8] = true, -- Attack Hit/Miss
                         [9] = true, -- Legacy
